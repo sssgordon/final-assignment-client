ae9acdeecee08d09e7fce3f05da5cfe35a67f2b2
sssgordon <gso7@uwo.ca>
2019-11-27 12:57:34 +0100

    Add comment actions and reducer

---
+++ src/actions/comments.js
@@ -0,0 +1,49 @@
+import request from "superagent";
+
+const baseUrl = "http://localhost:4000";
+
+// set comments
+export const SET_COMMENTS = "SET_COMMENTS";
+function setComments(payload) {
+  return {
+    type: SET_COMMENTS,
+    payload
+  };
+}
+
+export const getComments = ticketId => dispatch => {
+  request(`${baseUrl}/tickets/${ticketId}/comments`)
+    .then(response => {
+      // console.log("response test", response);
+      const action = setComments(response.body);
+      dispatch(action);
+    })
+    .catch(console.error);
+};
+
+// create comment
+export const NEW_COMMENT = "NEW_COMMENT";
+function newComment(payload) {
+  return {
+    type: NEW_COMMENT,
+    payload
+  };
+}
+
+export const createComment = () =>
+  // description,
+  // imageUrl,
+  // price,
+  // eventId,
+  // jwt
+  dispatch => {
+    request
+      .post(`${baseUrl}/comments`)
+      // .send({ description, imageUrl, price, eventId, jwt })
+      .then(response => {
+        const action = newComment(response.body);
+
+        dispatch(action);
+      })
+      .catch(console.error);
+  };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from "redux";
 import jwt from "./jwt";
 import events from "./events";
 import tickets from "./tickets";
+import comments from "./comments";
 
-export default combineReducers({ jwt, events, tickets });
+export default combineReducers({ jwt, events, tickets, comments });
