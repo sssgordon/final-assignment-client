0ee6c046c5d7ad98a9e5241bd9140c23c1257b53
sssgordon <gso7@uwo.ca>
2019-11-28 17:21:15 +0100

    Navbar css

--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -1,23 +1,31 @@
 import React from "react";
 import { Navbar, Nav, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 function NavButton(props) {
   if (props.jwt) {
-    return <Button variant="dark">User</Button>; // need link and display username (pass as props below)?
+    return (
+      <Button variant="light" className="welcome">
+        Welcome
+      </Button>
+    ); // need link and display username (pass as props below)?
   }
+
   return (
     <Link to="/login">
-      <Button variant="primary">Login</Button>
+      <Button variant="dark" className="login-btn">
+        Login
+      </Button>
     </Link>
   );
 }
 
 export default function Navigation(props) {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" className="bar">
       <Link to="/events">
-        <Navbar.Brand style={{ color: "#0275d8" }}>TicketExchange</Navbar.Brand>
+        <Navbar.Brand className="brand">TicketExchange</Navbar.Brand>
       </Link>
       <Nav className="mr-auto"></Nav>
       <NavButton jwt={props.jwt} />
